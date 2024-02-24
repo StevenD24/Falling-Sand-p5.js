c4211fc11286ba8ce30a697bdab1d12fbdf63f0f
Steven Duong <stevenduongx@gmail.com>
2024-02-24 10:01:10 -0700

    feat: setup 2D array grid

--- sketch.js
+++ sketch.js
@@ -1,8 +1,39 @@
+function make2DArray(cols, rows) {
+  arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+let grid;
+let w = 10;
+let cols, rows;
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(600, 600);
+  cols = width / 10;
+  rows = height / 10;
+  grid = make2DArray(cols, rows);
+
+  // initialize every grid index as 0
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = 0;
+    }
+  }
 }
 
 function draw() {
-  background(220);
-  fill(0);
+  background(0);
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      stroke(255);
+      fill(grid[i][j] * 255);
+      let x = i * w;
+      let y = j * w;
+      square(x, y, w);
+    }
+  }
 }
