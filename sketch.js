99a2d071c8ceabdb64b40ae14a29640728c0dc4b
Steven Duong <stevenduongx@gmail.com>
2024-02-24 16:47:52 -0700

    feat: add gravitational effect to the sand particles

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
+// Create a 2D array
 function make2DArray(cols, rows) {
   let arr = new Array(cols);
   for (let i = 0; i < arr.length; i++) {
     arr[i] = new Array(rows);
+    // Fill the array with 0s
     for (let j = 0; j < arr[i].length; j++) {
       arr[i][j] = 0;
     }
@@ -9,60 +11,68 @@ function make2DArray(cols, rows) {
   return arr;
 }
 
+// The grid
 let grid;
-let w = 10;
+let velocityGrid;
+
+// How big is each square?
+let w = 5;
 let cols, rows;
 let hueValue = 200;
 
+let gravity = 0.1;
+
+// Check if a row is within the bounds
 function withinCols(i) {
   return i >= 0 && i <= cols - 1;
 }
 
+// Check if a column is within the bounds
 function withinRows(j) {
   return j >= 0 && j <= rows - 1;
 }
 
 function setup() {
-  createCanvas(700, 800);
+  createCanvas(600, 800);
   colorMode(HSB, 360, 255, 255);
-  cols = width / 10;
-  rows = height / 10;
+  cols = width / w;
+  rows = height / w;
   grid = make2DArray(cols, rows);
-
-  // initialize every grid index as 0
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      grid[i][j] = 0;
-    }
-  }
+  velocityGrid = make2DArray(cols, rows, 1);
 }
 
-function mouseDragged() {
-  let mouseCol = floor(mouseX / w);
-  let mouseRow = floor(mouseY / w);
-
-  let matrix = 3;
-  let extent = floor(matrix / 2);
-  for (let i = -extent; i <= extent; i++) {
-    for (let j = -extent; j <= extent; j++) {
-      if (random(1) < 0.75) {
-        let col = mouseCol + i;
-        let row = mouseRow + j;
-        if (withinCols(col) && withinRows(row)) {
-          grid[col][row] = hueValue;
-        }
-      }
-    }
-  }
-  hueValue += 1;
-  if (hueValue > 360) {
-    hueValue = 1;
-  }
-}
+function mouseDragged() {}
 
 function draw() {
   background(0);
 
+  if (mouseIsPressed) {
+    let mouseCol = floor(mouseX / w);
+    let mouseRow = floor(mouseY / w);
+
+    // Randomly add an area of sand particles
+    let matrix = 5;
+    let extent = floor(matrix / 2);
+    for (let i = -extent; i <= extent; i++) {
+      for (let j = -extent; j <= extent; j++) {
+        if (random(1) < 0.75) {
+          let col = mouseCol + i;
+          let row = mouseRow + j;
+          if (withinCols(col) && withinRows(row)) {
+            grid[col][row] = hueValue;
+            velocityGrid[col][row] = 1;
+          }
+        }
+      }
+    }
+    // Change the color of the sand over time
+    hueValue += 0.5;
+    if (hueValue > 360) {
+      hueValue = 1;
+    }
+  }
+
+  // Draw the sand
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       noStroke();
@@ -75,38 +85,55 @@ function draw() {
     }
   }
 
+  // Create a 2D array for the next frame of animation
   let nextGrid = make2DArray(cols, rows);
+  let nextVelocityGrid = make2DArray(cols, rows);
+
+  // Check every cell
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
+      // What is the state?
       let state = grid[i][j];
+      let velocity = velocityGrid[i][j];
+      let moved = false;
       if (state > 0) {
-        let below = grid[i][j + 1];
-        let dir = random([-1, 1]);
+        let newPos = int(j + velocity);
+        for (let y = newPos; y > j; y--) {
+          let below = grid[i][y];
+          let dir = 1;
+          if (random(1) < 0.5) {
+            dir *= -1;
+          }
+          let belowA = -1;
+          let belowB = -1;
+          if (withinCols(i + dir)) belowA = grid[i + dir][y];
+          if (withinCols(i - dir)) belowB = grid[i - dir][y];
 
-        let belowL = -1;
-        let belowR = -1;
-
-        if (withinCols(i + dir)) {
-          belowL = grid[i + dir][j + 1];
+          if (below === 0) {
+            nextGrid[i][y] = state;
+            nextVelocityGrid[i][y] = velocity + gravity;
+            moved = true;
+            break;
+          } else if (belowA === 0) {
+            nextGrid[i + dir][y] = state;
+            nextVelocityGrid[i + dir][y] = velocity + gravity;
+            moved = true;
+            break;
+          } else if (belowB === 0) {
+            nextGrid[i - dir][y] = state;
+            nextVelocityGrid[i - dir][y] = velocity + gravity;
+            moved = true;
+            break;
+          }
         }
-        if (withinCols(i - dir)) {
-          belowR = grid[i - dir][j + 1];
-        }
+      }
 
-        belowL = grid[i - dir][j + 1];
-        belowR = grid[i + dir][j + 1];
-
-        if (below === 0) {
-          nextGrid[i][j + 1] = state;
-        } else if (belowL === 0) {
-          nextGrid[i - dir][j + 1] = state;
-        } else if (belowR === 0) {
-          nextGrid[i + dir][j + 1] = state;
-        } else {
-          nextGrid[i][j] = state;
-        }
+      if (state > 0 && !moved) {
+        nextGrid[i][j] = grid[i][j];
+        nextVelocityGrid[i][j] = velocityGrid[i][j] + gravity;
       }
     }
   }
   grid = nextGrid;
+  velocityGrid = nextVelocityGrid;
 }
